d20a880e5b4b1eaa567b8ba0b10d363272ddaa2b
@Slayer41 <ksealmalaysia41@gmail.com>
2026-02-02 15:03:18 +0800

    Initialize nanosvg-fuzzing

---
+++ projects/lxml/fuzz_sax.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import atheris
+import sys
+import io
+
+with atheris.instrument_imports():
+  from lxml import sax, etree
+
+
+def TestOneInput(data):
+  try:
+    f = io.BytesIO(data)
+    parsed = etree.parse(f)
+
+    handler = sax.ElementTreeContentHandler()
+    sax.ElementTreeProducer(parsed, handler).saxify()
+  except (etree.LxmlError, ValueError) as e:
+    if isinstance(e, etree.LxmlError) or (isinstance(e, ValueError) and
+                                          "Invalid" in str(e)):
+      return -1  # Reject so the input will not be added to the corpus.
+    else:
+      raise e
+
+
+def main():
+  atheris.Setup(sys.argv, TestOneInput)
+  atheris.Fuzz()
+
+
+if __name__ == "__main__":
+  main()
