6985bda13fc4c99db13f4daa52993d3160e237d9
@Slayer41 <ksealmalaysia41@gmail.com>
2026-02-02 14:54:22 +0800

    Initialize upgrade-image-disable

---
+++ projects/net-snmp/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -eu
+# Copyright 2018 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+# build project
+./configure --with-openssl=/usr --with-defaults --with-logfile="/dev/null" --with-persistent-directory="/dev/null"
+# net-snmp build is not parallel-make safe; do not add -j
+make
+
+# build fuzzers and link statically
+fuzzers=$(find ./testing/fuzzing -name "*_fuzzer.c")
+suffix="_fuzzer\.c"
+for fuzzer in ${fuzzers}; do
+  fuzzname=$(basename -- ${fuzzer%$suffix})
+  $CC $CFLAGS -c -Iinclude -Iagent/mibgroup/agentx ./testing/fuzzing/${fuzzname}_fuzzer.c -o $WORK/${fuzzname}_fuzzer.o
+  $CXX $CXXFLAGS $WORK/${fuzzname}_fuzzer.o \
+        $LIB_FUZZING_ENGINE snmplib/.libs/libnetsnmp.a \
+        agent/.libs/libnetsnmpagent.a \
+        -Wl,-Bstatic -lcrypto -Wl,-Bdynamic -lm \
+        -o $OUT/${fuzzname}_fuzzer
+done
