15125131917398af0056910646ee9c8982421327
@Slayer41 <ksealmalaysia41@gmail.com>
2026-02-02 15:02:43 +0800

    Initialize base-runner-bump-python

---
+++ infra/base-images/base-runner/install_deps.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -eux
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+# Install dependencies in a platform-aware way.
+
+apt-get update && apt-get install -y \
+    binutils \
+    curl \
+    file \
+    fonts-dejavu \
+    git \
+    libcap2 \
+    rsync \
+    unzip \
+    wget \
+    zip --no-install-recommends
+
+# Install Python3.10 as this is needed to extract code coverage for Python
+# projects that require 3.10+ syntax.
+apt-get update -y \
+    && apt-get install -y libgdal26 software-properties-common \
+    && add-apt-repository -y ppa:deadsnakes/ppa \
+    && apt-get install -y python3.10 python3.10-dev \
+    && ln --force -s /usr/bin/python3.10 /usr/local/bin/python3 \
+    && curl -sS https://bootstrap.pypa.io/get-pip.py | python3
+
+case $(uname -m) in
+  x86_64)
+    # We only need to worry about i386 if we are on x86_64.
+    apt-get install -y lib32gcc1 libc6-i386
+    ;;
+esac
---
+++ projects/nodejs/build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -eu
+# Copyright 2020 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+cd $SRC/node
+
+# Coverage build takes very long and time outs in the CI which blocks changes. Ignore Coverage build in OSS-Fuzz CI for now:
+if [[ -n "${OSS_FUZZ_CI-}" && "$SANITIZER" = coverage ]]; then
+	exit 0
+fi
+
+# Build node
+export LDFLAGS="$CXXFLAGS"
+export LD="$CXX"
+./configure --with-ossfuzz
+if [[ "$SANITIZER" = coverage ]]; then
+	make
+else
+	make -j$(nproc)
+fi
+
+# Copy all fuzzers to OUT folder 
+cp out/Release/fuzz_* ${OUT}/
+
+# Create seed for fuzz_env
+mkdir fuzz_env_seed
+find ./test -name '*.js' -exec cp {} ./fuzz_env_seed/ \;
+cd fuzz_env_seed
+# Remove small files:
+find -size -5k -delete
+# Remove large files:
+find -size +30k -delete
+zip $OUT/fuzz_env_seed_corpus.zip ./*
